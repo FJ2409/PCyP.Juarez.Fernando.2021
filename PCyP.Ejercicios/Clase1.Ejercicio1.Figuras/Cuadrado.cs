167a2910f204e89ff76ecf74391a6fc6d812f8be
Fernando Juarez <suku_fer22@hotmail.com>
2021-03-26 18:01:12 -0300

    implementando mas figuras
    implemente triangulo y cuadrado

--- PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Cuadrado.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Cuadrado.cs
@@ -8,5 +8,35 @@ namespace Clase1.Ejercicio1.Figuras
 {
     class Cuadrado
     {
+        private int lado;
+        
+        public Cuadrado()
+        {
+            this.lado = 0;
+        }
+
+        public void setLado(int lado)
+        {
+            this.lado = lado;
+        }
+
+        public int getLado()
+        {
+            return lado;
+        }
+
+        public int calcularArea()
+        {
+            int area = 0;
+
+            return area = lado * lado;
+        }
+        
+        public int calcularPerimetro()
+        {
+            int perimetro = 0;
+
+            return perimetro = lado*4;
+        }   
     }
 }
--- PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Triangulo.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Triangulo.cs
@@ -8,5 +8,55 @@ namespace Clase1.Ejercicio1.Figuras
 {
     class Triangulo
     {
+        int lado1, lado2, base1, altura;
+
+        public Triangulo()
+        {
+            this.lado1 = 0;
+            this.lado2 = 0;
+            this.base1 = 0;
+            this.altura = 0;
+        }
+
+        public void setLado1(int lado1)
+        {
+            this.lado1 = lado1;
+
+        }
+
+        public int getLado1()
+        {
+            return lado1;
+        }
+
+        public void setLado2(int lado2)
+        {
+            this.lado2 = lado2;
+        }
+
+        public int getLado2()
+        {
+            return lado2;
+        }
+
+        public void setBase1(int base1)
+        {
+            this.base1 = base1;
+        }
+
+        public int getBase1()
+        {
+            return base1;
+        }
+
+        public int calcularArea()
+        {
+            int area=0;
+
+
+            return area= (base1*altura)/2;
+
+        }
     }
+
 }
