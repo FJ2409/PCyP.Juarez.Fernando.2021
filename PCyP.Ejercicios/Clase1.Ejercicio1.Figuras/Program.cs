bb5256d0ac51e0a2cb39aaf158f89cef4ccf1f75
Fernando Juarez <suku_fer22@hotmail.com>
2021-03-26 14:05:19 -0300

    Creando clases de figuras
    Cree las clases de las tres figuras del ejercicio, se implemento Área y Perímetro a la clase Rectángulo

---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo r = new Rectangulo();
+
+            r.setBase1(13);
+            r.setAltura1(216);
+            Console.WriteLine(r.calcularArea());
+
+
+            Console.ReadKey();
+        }
+
+    }
+    }
+
---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Rectangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figuras
+{
+    class Rectangulo
+    {
+        private int base1;
+        private int altura1;
+
+
+
+        public Rectangulo()
+        {
+            this.base1 = 0;
+            this.altura1 = 0;
+        }
+
+        public void setBase1(int base1)
+        {
+            this.base1 = base1;
+        }
+
+        public int getBase1()
+        {
+            return base1;
+        }
+
+        public void setAltura1(int altura1)
+        {
+            this.altura1 = altura1;
+        }
+
+        public int getAltura1()
+        {
+            return altura1;
+        }
+
+        public int calcularArea()
+        {
+            int area = 0;
+
+            return area = base1 * altura1;
+
+        }
+
+        public int calcularPerimetro()
+        {
+            int perimetro = 0;
+
+            return perimetro = base1 * 2 + altura1 * 2;
+        }
+    }
+
+}
