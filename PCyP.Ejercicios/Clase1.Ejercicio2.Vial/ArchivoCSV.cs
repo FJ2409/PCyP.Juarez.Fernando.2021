f95a646130803ae0234bd6061afbe95d0e325561
Fernando Juarez <suku_fer22@hotmail.com>
2021-04-01 17:40:59 -0300

    Ejercicios Finalizados

---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/ArchivoCSV.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    class ArchivoCSV
+    {
+        private int numeroDeHoja;
+        private int identificadorTramo;
+        private int longitud;
+        private string tipoObjeto;
+        private string tipoCamino;
+
+
+
+        public ArchivoCSV()
+        {
+
+        }
+
+        public ArchivoCSV(int numeroDeHoja, string tipoObjeto, int identificadorTramo, string tipoCamino, int longitud)
+        {
+            this.numeroDeHoja = numeroDeHoja;
+            this.identificadorTramo = identificadorTramo;
+            this.longitud = longitud;
+            this.tipoObjeto = tipoObjeto;
+            this.tipoCamino = tipoCamino;
+        }
+
+        public void setNumeroDeHoja(int numeroDeHoja)
+        {
+            this.numeroDeHoja = numeroDeHoja;
+        }
+
+        public int getNumeroDeHoja()
+        {
+            return numeroDeHoja;
+        }
+
+        public void setIdentificadorTramo(int identificadorTramo)
+        {
+            this.identificadorTramo = identificadorTramo;
+        }
+
+        public int getIdentificadorTramo()
+        {
+            return identificadorTramo;
+        }
+
+        public void setLongitud(int longitud)
+        {
+            this.longitud = longitud;
+        }
+
+        public int getLongitud()
+        {
+            return longitud;
+        }
+
+        public void setTipoObjeto (String tipoObjeto)
+        {
+            this.tipoObjeto = tipoObjeto;
+        }
+
+        public String getTipoObjeto()
+        {
+            return tipoObjeto;
+        }
+
+        public void setTipoCamino (String tipoCamino)
+        {
+            this.tipoCamino = tipoCamino;
+        }
+
+        public String getTipoCamino()
+        {
+            return tipoCamino;
+        }
+
+
+        public void imprimir()
+        {
+            Console.WriteLine("\nNumero de Hoja: {0}\nTipo de Objeto: {1} \nTipo de Camino: {2}\nIdentificador de Tramo: {3}\nLongitud: {4}", numeroDeHoja, tipoObjeto, identificadorTramo, tipoCamino, longitud);
+        }
+    }
+}
---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] archivo= File.ReadAllLines("C:/Users/FJUAREZ/Desktop/vial.csv");
+
+            ArchivoCSV csv = new ArchivoCSV();
+            int cont = 0;
+
+
+            foreach (var linea in archivo)
+            {
+                var campo = linea.Split(',');
+                csv = new ArchivoCSV(int.Parse(campo[0]), campo[1] , int.Parse(campo[2]), campo[3], int.Parse(campo[4]));
+                csv.imprimir();
+                cont++;
+            }
+
+            Console.WriteLine("\nRegistros: " + cont);
+            Console.ReadLine();
+        }
+    }
+}
